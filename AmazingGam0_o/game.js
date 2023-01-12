157d41d3711c0439142cc845b5eb644389c0361d
soonpepper <vii.mjay@gmail.com>
2023-01-12 12:05:43 -0500

    thing

---
+++ AmazingGam0_o/game.js
@@ -0,0 +1,113 @@
+function hi(){
+    alert("Hi this is the game!");
+}
+
+var canvas = document.getElementById("canva");
+var ctx = canvas.getContext("2d");
+
+var ballRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+var paddleHeight = 30;
+var paddleWidth = 30;
+var paddleX = (canvas.width-paddleWidth)/2;
+var paddleY = (canvas.height-paddleHeight)/2;
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false; 
+
+
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+    else if(e.key == "Up" || e.key == "ArrowUp") {
+        upPressed = true;
+    }
+    else if(e.key == "Down" || e.key == "ArrowDown") {
+        downPressed = true;
+    }
+}
+
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    } else if (e.key == "Up" || e.key == "ArrowUp") {
+        upPressed = false;
+    } else if (e.key == "Down" || e.key == "ArrowDown") {
+        downPressed = false;
+    }
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //drawBall();
+    drawPaddle();
+    
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    
+    if(rightPressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if(leftPressed) {
+        paddleX -= 7;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
+    }
+    else if(upPressed) {
+        paddleY -= 7;
+        if (paddleY < 0){
+            paddleY = 0;
+        }
+    }
+    else if(downPressed) {
+        paddleY += 7;
+        if (paddleY + paddleHeight > canvas.height){
+            paddleY = canvas.height - paddleHeight;
+        }
+    }
+    
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 0.001);
